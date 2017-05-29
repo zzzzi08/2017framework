fc7a0d00fb8e163de7c55f14fcdf3e57356daac4
1027 <1027@hb6013>
2017-05-29 12:12:27 +0900

    DispatcherServlet에서 properties분리

--- day03/src/main/java/com/hb/day03/support/DispatcherServlet.java
+++ day03/src/main/java/com/hb/day03/support/DispatcherServlet.java
@@ -1,6 +1,10 @@
 package com.hb.day03.support;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,18 +20,33 @@ public class DispatcherServlet extends HttpServlet{
 	public void init() throws ServletException {
 		super.init();
 		
-		HendlerMapping.setMap("/main.do", "com.hb.day03.controller.SelectAll");
+//		HendlerMapping.setMap("/main.do", "com.hb.day03.controller.SelectAll"); //이거대신에 propeties파일에서 읽어오게
+		
+		Properties prop = new Properties();
+		InputStream is = getClass().getClassLoader().getResourceAsStream("bean.properties");
+		if(is!=null){
+			try {
+				prop.load(is);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		Set<Object> obj = prop.keySet();
+		Iterator<Object> ite = obj.iterator();
+		while(ite.hasNext()){
+			String key = (String) ite.next();
+			String value = prop.getProperty(key);
+			HendlerMapping.setMap(key, value);
+		}
 		
 		/*
 		list	list.do		get
 		detail	detail.do	get
-
-
+		
 		insert	update.do	post
 		update	update.do	put
 		delete	update.do	delete
 		*/
-
 		
 	}
 	
