1d8c92493f193852064d756f42d8cb12034f9ef3
1027 <1027@hb6013>
2017-06-02 11:07:39 +0900

    Spring 2.5~ (tool : 이클립스) - (1)

---
+++ sts01/src/com/hb/day01/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.hb.day01.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		System.out.println("indexController ½ÇÇà");
+		return null;
+	}
+
+}
